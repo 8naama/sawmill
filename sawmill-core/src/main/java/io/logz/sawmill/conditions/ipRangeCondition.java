586a2332de3c59fc35085b11af84d97d158499e1
Naama Bendalak <naama@Naamas-MacBook-Pro.local>
2023-01-16 17:34:34 +0200

    add tests and config field existance check

--- sawmill-core/src/main/java/io/logz/sawmill/conditions/ipRangeCondition.java
+++ sawmill-core/src/main/java/io/logz/sawmill/conditions/ipRangeCondition.java
@@ -17,13 +17,21 @@ public class IPRangeCondition implements Condition {
     private final IPAddress rangeEndIP;
 
     public IPRangeCondition(String field, String startIP, String endIP) {
-        this.field = field;
+		if (field == null) {
+			throw new ProcessorConfigurationException("failed to parse IPRange condition, missing field");
+		}
+
+        if (IPAddressString(startIP).getAddress() == null) {
+            throw new ProcessorConfigurationException("failed to parse ipRange condition, invlid IP provided in rangeStartIP");
+        }
+
+		if (IPAddressString(endIP).getAddress() == null) {
+            throw new ProcessorConfigurationException("failed to parse ipRange condition, invlid IP provided in rangeEndIP");
+        }
+
+		this.field = field;
         this.rangeStartIP = new IPAddressString(startIP).getAddress();
         this.rangeEndIP = new IPAddressString(endIP).getAddress();
-
-        if (this.rangeStartIP == null || this.rangeEndIP == null) {
-            throw new ProcessorConfigurationException("failed to parse ipRange condition, invlid IP provided in rangeStartIP or rangeEndIP");
-        }
     }
 
     @Override
--- sawmill-core/src/test/java/io/logz/sawmill/conditions/IpRangeConditionTest.java
+++ sawmill-core/src/test/java/io/logz/sawmill/conditions/IpRangeConditionTest.java
@@ -1,7 +1,12 @@
 package io.logz.sawmill.conditions;
 
+import io.logz.sawmill.exceptions.ProcessorConfigurationException;
 import io.logz.sawmill.Doc;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.logz.sawmill.utils.DocUtils.createDoc;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,17 +14,59 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class IPRangeConditionTest {
 	
 	@Test
-	public void testFieldNotExists() {}
+	public void invalidConfig() {
+		IPRangeCondition.Factory factory = new IPRangeCondition.Factory();
+		Map<String,Object> config = new HashMap<>();
+		config.put("field", "fieldName");
+        config.put("rangeStartIP", "5.53.255.255");
+		config.put("rangeEndIP", "ffff:200.100.2.2");
+
+		// check invalid endIp
+		assertThatThrownBy(() ->factory.create(config, null)).isInstanceOf(ProcessorConfigurationException.class)
+                .hasMessageContaining("failed to parse ipRange condition, invlid IP provided in rangeEndIP");
+
+		// check invalid startIP
+		config.put("rangeStartIP", "");
+		assertThatThrownBy(() ->factory.create(config, null)).isInstanceOf(ProcessorConfigurationException.class)
+                .hasMessageContaining("failed to parse ipRange condition, invlid IP provided in rangeStartIP");
+		
+		// check missing field
+		assertThatThrownBy(() -> new IPRangeCondition(null, "ffff:200.100.2.2", "5.53.255.255")).isInstanceOf(ProcessorConfigurationException.class)
+                .hasMessageContaining("failed to parse IPRange condition, missing field");
+		
+		// check missing ip value
+		assertThatThrownBy(() -> new IPRangeCondition("path", null, "5.53.255.255")).isInstanceOf(ProcessorConfigurationException.class)
+                .hasMessageContaining("failed to parse ipRange condition, invlid IP provided in rangeStartIP");
+	}
 
 	@Test
-	public void testNotIPValue() {}
+	public void testNotIPValue() {
+		FieldTypeCondition isInRange = new FieldTypeCondition("testField", "192.158.0.0", "192.158.255.255");
+
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("otherField", "otherValue"))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", "not an IP"))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", 12345))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", null))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", -4.5))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", 4.5))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", ""))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", "12345"))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", -10))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", new ArrayList<>()))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", new HashMap<>()))).isFalse();
+		
+	}
 
 	@Test
-	public void testvalidPublicIPValue() {}
+	public void testvalidIPValue() {
+		FieldTypeCondition isInRange = new FieldTypeCondition("testField", "192.158.0.0", "192.158.255.255");
 
-	@Test
-	public void testvalidPrivateIPValue() {}
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", "192.159.38.4"))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", "255.255.255.255"))).isFalse();
+		assertThat(FieldTypeCondition.evaluate(DocUtils.createDoc("testField", "0.0.0.0"))).isFalse();
 
-	@Test
-	public void testvEmptyValue() {}
+		assertThat(isTypeCondition.evaluate(DocUtils.createDoc("testField", "192.158.38.4"))).isTrue();
+		assertThat(isTypeCondition.evaluate(DocUtils.createDoc("testField", "192.158.0.0"))).isTrue();
+		assertThat(isTypeCondition.evaluate(DocUtils.createDoc("testField", "192.158.255.255"))).isTrue();
+	}
 }
